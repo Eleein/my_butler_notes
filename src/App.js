e2b5a1aeb0b7d55bac9e3fa98b424192a361ef61
Dania Duany <deduany@gmail.com>
2020-09-26 08:33:25 +0200

    Routing 	-Add react-router-dom 	-Configure Routes 	-Create 404 page
    Reported-by:Dania-Duany

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { useContext } from "react";
+import { Switch, Route } from "react-router-dom";
 import { AppContext } from "./AppContext";
+import {PageNotFound} from "./Page_not_found/Page_not_found";
 
 function App() {
   const {
     globalState: { name },
   } = useContext(AppContext);
 
+  const paths = { login: "login", notes: "notes" };
   return (
     <div className="App">
       <header className="App-header">{name}</header>
+      <Switch>
+        <Route exact path={paths.login}>
+          {/*Here Login Component*/}
+        </Route>
+        <Route exact path={paths.notes}>
+          {/* Here Notes Component*/}
+        </Route>
+          <Route path="*">
+
+          <PageNotFound/>
+          </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/Page_not_found/Page_not_found.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function PageNotFound(){
+    return <h1>PAGE NOT FOUND: 404</h1>
+}
